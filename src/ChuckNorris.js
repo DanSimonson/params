7a30643825de316427745091bc7927d3c97b44df
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2019-03-16 23:27:42 +0800

    first draft of param id linking finished

--- src/ChuckNorris.js
+++ src/ChuckNorris.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
-//import Home from './Home'
+import axios from 'axios'
 class ChuckNorris extends Component {
   state = {
-    id: ''
+    id: '',
+    norrisjoke: ''
   }
 
   componentDidMount() {
     console.log(this.props)
     let id = this.props.match.params.id
     this.setState({ id: id })
+    //http://api.icndb.com/jokes/15
+    axios.get('http://api.icndb.com/jokes/'+ id)
+      .then(response => {
+        //this.setState({ facts: response });
+        console.log('response: ',response)
+        console.log(response.data.value.joke)
+        this.setState({norrisjoke:response.data.value.joke})
+        //console.log('fact: ', this.state.facts)
+        //console.log(this.state.facts[0].id)
+        //console.log(this.state.facts[0].joke)
+
+      }).catch(error => {
+        console.log(error);
+      })
 
   }
   render() {
     let myId = this.state.id
     return (
       <div className="container">
-        <h4>route params: {this.props.match.params.id}</h4>
+        <h4>Chuck Norris Joke ID: {this.state.id}</h4>
+        <h1>{this.state.norrisjoke}</h1>
+
       </div>
     )
   }
